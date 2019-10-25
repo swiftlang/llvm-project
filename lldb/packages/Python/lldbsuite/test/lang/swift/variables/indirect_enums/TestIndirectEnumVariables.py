0336ecdcf235607b6014bfb9b6d88ba22db89125
Mishal Shah <shahmishal@users.noreply.github.com>
2019-10-24 23:03:46 -0700

    Skip TestIndirectEnumVariables.py tests on macOS (56608695)

--- lldb/packages/Python/lldbsuite/test/lang/swift/variables/indirect_enums/TestIndirectEnumVariables.py
+++ lldb/packages/Python/lldbsuite/test/lang/swift/variables/indirect_enums/TestIndirectEnumVariables.py
@@ -25,14 +25,14 @@ class TestIndirectEnumVariables(TestBase):
     mydir = TestBase.compute_mydir(__file__)
 
     @swiftTest
-    @skipIf(bugnumber='rdar://27568868', oslist=['linux'])
+    @skipIf(bugnumber='rdar://27568868&56608695', oslist=['linux', 'macosx'])
     def test_indirect_cases_variables(self):
         """Tests that indirect Enum variables display correctly when cases are indirect"""
         self.build()
         self.do_test("indirect case break here")
 
     @swiftTest
-    @skipIf(bugnumber='rdar://27568868', oslist=['linux'])
+    @skipIf(bugnumber='rdar://27568868&56608695', oslist=['linux', 'macosx'])
     def test_indirect_enum_variables(self):
         """Tests that indirect Enum variables display correctly when enum is indirect"""
         self.build()
