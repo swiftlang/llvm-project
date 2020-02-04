3c674a92be8c4d324771520463ae8b382359a714
Robert Widmann <devteam.codafi@gmail.com>
2020-02-04 14:22:58 -0800

    Adjust for apple/swift#29631

--- lldb/source/Symbol/SwiftASTContext.cpp
+++ lldb/source/Symbol/SwiftASTContext.cpp
@@ -3551,6 +3551,7 @@ swift::ASTContext *SwiftASTContext::GetASTContext() {
   registerParseRequestFunctions(m_ast_context_ap->evaluator);
   registerTypeCheckerRequestFunctions(m_ast_context_ap->evaluator);
   registerSILGenRequestFunctions(m_ast_context_ap->evaluator);
+  registerIRGenRequestFunctions(m_ast_context_ap->evaluator);
 
   GetASTMap().Insert(m_ast_context_ap.get(), this);
 
