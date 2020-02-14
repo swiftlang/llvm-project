48b60ba6c3bf1750254dc2a633bcad6bd7681c47
Davide Italiano <ditaliano@apple.com>
2020-02-13 17:53:08 -0800

    [TestHelp] The string returned by version can contain `Apple`.
    Take that in account to make this test more robust.

--- lldb/packages/Python/lldbsuite/test/commands/help/TestHelp.py
+++ lldb/packages/Python/lldbsuite/test/commands/help/TestHelp.py
@@ -89,7 +89,7 @@ class HelpCommandTestCase(TestBase):
         version_str = self.version_number_string()
         match = re.match('[0-9]+', version_str)
         search_regexp = ['lldb( version|-' + (version_str if match else '[0-9]+') + '| \(swift-.*\)).*\n']
-        search_regexp[0] += 'Swift'
+        search_regexp[0] += '[Apple]* Swift'
 
         self.expect("version",
                     patterns=search_regexp)
