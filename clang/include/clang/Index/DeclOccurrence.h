5cb1b86c254b0988f5ef6fd4d6937089359aad07
Ben Langmuir <blangmuir@apple.com>
2021-03-23 15:22:58 -0700

    [index] Improve macro indexing support
    The major change here is to index macro occurrences in more places than
    before, specifically
    
    * In non-expansion references such as `#if`, `#ifdef`, etc.
    * When the macro is a reference to a builtin macro such as __LINE__.
    * When using the preprocessor state instead of callbacks, we now include
      all definition locations and undefinitions instead of just the latest
      one (which may also have had the wrong location previously).
    * When indexing an existing module file (.pcm), we now include module
      macros, and we no longer report unrelated preprocessor macros during
      indexing the module, which could have caused duplication.
    
    Additionally, we now correctly obey the system symbol filter for macros,
    so by default in system headers only definition/undefinition occurrences
    are reported, but it can be configured to report references as well if
    desired.
    
    Extends FileIndexRecord to support occurrences of macros. Since the
    design of this type is to keep a single list of entities organized by
    source location, we incorporate macros into the existing DeclOccurrence
    struct.

--- clang/include/clang/Index/DeclOccurrence.h
+++ clang/include/clang/Index/DeclOccurrence.h
@@ -12,23 +12,29 @@
 #include "clang/Basic/LLVM.h"
 #include "clang/Index/IndexSymbol.h"
 #include "llvm/ADT/ArrayRef.h"
+#include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/SmallVector.h"
 
 namespace clang {
 class Decl;
+class MacroInfo;
 
 namespace index {
 
 struct DeclOccurrence {
   SymbolRoleSet Roles;
   unsigned Offset;
-  const Decl *Dcl;
+  llvm::PointerUnion<const Decl *, const MacroInfo *> DeclOrMacro;
+  const IdentifierInfo *MacroName = nullptr;
   SmallVector<SymbolRelation, 3> Relations;
 
   DeclOccurrence(SymbolRoleSet R, unsigned Offset, const Decl *D,
                  ArrayRef<SymbolRelation> Relations)
-      : Roles(R), Offset(Offset), Dcl(D),
+      : Roles(R), Offset(Offset), DeclOrMacro(D),
         Relations(Relations.begin(), Relations.end()) {}
+  DeclOccurrence(SymbolRoleSet R, unsigned Offset, const IdentifierInfo *Name,
+                 const MacroInfo *MI)
+      : Roles(R), Offset(Offset), DeclOrMacro(MI), MacroName(Name) {}
 
   friend bool operator<(const DeclOccurrence &LHS, const DeclOccurrence &RHS) {
     return LHS.Offset < RHS.Offset;
