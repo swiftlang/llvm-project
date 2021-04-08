fc99d966edd13d3ca3abf14aa845d51891adca0f
Dave Lee <davelee.com@gmail.com>
2021-04-07 18:21:39 -0700

    fix indentation

--- lldb/test/API/lang/swift/async/frame/TestSwiftAsyncFrameVar.py
+++ lldb/test/API/lang/swift/async/frame/TestSwiftAsyncFrameVar.py
@@ -13,8 +13,8 @@ class TestCase(lldbtest.TestBase):
         """Test `frame variable` in async functions"""
         self.build()
 
-	# Setting a breakpoint on "main" results in a breakpoint at the start
-	# of each coroutine "funclet" function.
+        # Setting a breakpoint on "main" results in a breakpoint at the start
+        # of each coroutine "funclet" function.
         _, process, _, _ = lldbutil.run_to_name_breakpoint(self, 'main')
 
         while process.state == lldb.eStateStopped:
