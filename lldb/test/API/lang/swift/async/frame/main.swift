cf9f2649efecea254fae033358adc3027ccb0fcd
Dave Lee <davelee.com@gmail.com>
2021-04-08 15:46:32 -0700

    understand the seams better to improve the tests

--- lldb/test/API/lang/swift/async/frame/main.swift
+++ lldb/test/API/lang/swift/async/frame/main.swift
@@ -2,12 +2,19 @@ func randInt(_ i: Int) async -> Int {
   return Int.random(in: 1...i)
 }
 
+func inner() async {
+  let a = await randInt(30)
+  let b = await randInt(a + 11)
+  use(a, b)
+}
+
 func use<T>(_ t: T...) {}
 
 @main struct Main {
   static func main() async {
-    let a = await randInt(30)
-    let b = await randInt(a + 11)
-    use(a, b)
+    // This call to `inner` is a indirection required to make this test work.
+    // If its contents were inlined into `main` (as it was originally written),
+    // the test would fail.
+    await inner()
   }
 }
