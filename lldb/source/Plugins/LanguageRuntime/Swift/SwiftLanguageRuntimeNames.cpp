9370cfc8add9dfec99ef541589d7896e2e59e071
Dave Lee <davelee.com@gmail.com>
2021-05-10 15:44:56 -0700

    check node hasChildren too

--- lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeNames.cpp
+++ lldb/source/Plugins/LanguageRuntime/Swift/SwiftLanguageRuntimeNames.cpp
@@ -60,7 +60,7 @@ enum class ThunkAction {
 static swift::Demangle::NodePointer
 childAtPath(swift::Demangle::NodePointer node,
             llvm::ArrayRef<swift::Demangle::Node::Kind> path) {
-  if (!node || path.empty())
+  if (!node || !node->hasChildren() || path.empty())
     return node;
 
   auto current_step = path.front();
