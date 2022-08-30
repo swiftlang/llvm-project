cd75da84aa88d05a27bbe39cc2bf038585ebee4a
Ben Langmuir <blangmuir@apple.com>
2022-08-30 09:21:06 -0700

    [clang][cas] Create node schema for comilation cache result
    Factor the code for creating and reading a compilation result out of
    cc1_main and create a CompileJobCacheResult proxy for working with
    cached compilation results. This is in preparation for accessing cached
    module compilations.
    
    Also, make it a hard error to create an output file of unknown kind -
    previously these could create cache values that depended on output paths
    not in the cache key.

--- clang/lib/Tooling/DependencyScanning/DependencyScanningWorker.cpp
+++ clang/lib/Tooling/DependencyScanning/DependencyScanningWorker.cpp
@@ -23,6 +23,7 @@
 #include "clang/Tooling/DependencyScanning/ModuleDepCollector.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/CAS/CachingOnDiskFileSystem.h"
+#include "llvm/CAS/ObjectStore.h"
 #include "llvm/Support/Host.h"
 
 using namespace clang;
