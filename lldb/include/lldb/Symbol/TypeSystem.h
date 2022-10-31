b673f7c6f7e1d59ea37ecf14e510848d79e51ea7
Alex Langford <apl@fb.com>
2022-10-31 15:05:46 -0700

    [lldb] Add information on type systems to statistics dump command
    Context: I plan on using this change primarily downstream in the apple
    fork of llvm to track swift module loading time.

--- lldb/include/lldb/Symbol/TypeSystem.h
+++ lldb/include/lldb/Symbol/TypeSystem.h
@@ -19,6 +19,7 @@
 #include "llvm/ADT/SmallBitVector.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/Error.h"
+#include "llvm/Support/JSON.h"
 
 #include "lldb/Core/PluginInterface.h"
 #include "lldb/Expression/Expression.h"
@@ -535,6 +536,8 @@ public:
   // formatters that are discovered on such a type as attributable to the
   // meaningless type itself, instead preferring to use the dynamic type
   virtual bool IsMeaninglessWithoutDynamicResolution(void *type);
+
+  virtual llvm::Optional<llvm::json::Value> ReportStatistics();
 
   /// A TypeSystem may belong to more than one debugger, so it doesn't
   /// have a way to communicate errors. This method can be called by a
