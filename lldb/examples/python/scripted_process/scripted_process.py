2da6b238b0a80ab714bbf7d1790e9d39bbca9987
Med Ismail Bennani <medismail.bennani@gmail.com>
2023-02-01 14:52:50 -0800

    [lldb] Fix typo in ScriptedProcess python docstrings (NFC)
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- lldb/examples/python/scripted_process/scripted_process.py
+++ lldb/examples/python/scripted_process/scripted_process.py
@@ -196,7 +196,7 @@ class ScriptedProcess(metaclass=ABCMeta):
 
         Returns:
             Dict: A dictionary containing metadata for the scripted process.
-                  None is the process as no metadata.
+                  None if the process as no metadata.
         """
         return self.metadata
 
@@ -346,7 +346,7 @@ class ScriptedThread(metaclass=ABCMeta):
 
         Returns:
             List: A list containing the extended information for the scripted process.
-                  None is the thread as no extended information.
+                  None if the thread as no extended information.
         """
         return self.extended_info
 
