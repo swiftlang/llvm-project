95919b0be977060ebe4652cbf4c0ad4512fe370e
Shubham Sandeep Rastogi <srastogi22@apple.com>
2023-04-26 22:20:52 -0700

    test hack

--- llvm/lib/MC/MCDwarf.cpp
+++ llvm/lib/MC/MCDwarf.cpp
@@ -193,24 +193,24 @@ void MCDwarfLineTable::emitOne(
     if (LineEntry.IsEndEntry || LineEntry.IsEndOfFunction) {
       MCOS->emitDwarfAdvanceLineAddr(INT64_MAX, LastLabel, Label,
                                      asmInfo->getCodePointerSize());
-      unsigned PrevFileNum = FileNum;
+      // unsigned PrevFileNum = FileNum;
       init();
       // File number doesn't need to be reset and extra DW_LNS_set_file
       // directives reduces deduplication
-      if (LineEntry.IsEndOfFunction)
-        FileNum = PrevFileNum;
+      // if (LineEntry.IsEndOfFunction)
+        // FileNum = PrevFileNum;
       EndEntryEmitted = true;
       continue;
     }
 
     int64_t LineDelta = static_cast<int64_t>(LineEntry.getLine()) - LastLine;
 
-    if (FileNum != LineEntry.getFileNum()) {
+    if (FileNum != LineEntry.getFileNum() || LineEntry.IsEndOfFunction) {
       FileNum = LineEntry.getFileNum();
       MCOS->emitInt8(dwarf::DW_LNS_set_file);
       MCOS->emitULEB128IntValue(FileNum);
     }
-    if (Column != LineEntry.getColumn()) {
+    if (Column != LineEntry.getColumn() || LineEntry.IsEndOfFunction) {
       Column = LineEntry.getColumn();
       MCOS->emitInt8(dwarf::DW_LNS_set_column);
       MCOS->emitULEB128IntValue(Column);
