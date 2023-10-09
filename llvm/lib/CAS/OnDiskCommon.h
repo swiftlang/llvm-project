d389b77cc167743cbd42a230c5f6b701a9dc9096
Steven Wu <stevenwu@apple.com>
2023-10-09 09:07:44 -0700

    [next] Add change from upstream #114098
    Update the downstream code to use upstreamed content.

--- llvm/lib/CAS/OnDiskCommon.h
+++ llvm/lib/CAS/OnDiskCommon.h
@@ -11,6 +11,7 @@
 
 #include "llvm/ADT/SmallString.h"
 #include "llvm/Support/Error.h"
+#include "llvm/Support/FileSystem.h"
 #include <chrono>
 #include <optional>
 
@@ -30,7 +31,8 @@ LLVM_ABI_FOR_TEST void setMaxMappingSize(uint64_t Size);
 /// Thread-safe alternative to \c sys::fs::lockFile. This does not support all
 /// the platforms that \c sys::fs::lockFile does, so keep it in the CAS library
 /// for now.
-std::error_code lockFileThreadSafe(int FD, bool Exclusive = true);
+std::error_code lockFileThreadSafe(
+    int FD, llvm::sys::fs::LockKind Kind = llvm::sys::fs::LockKind::Exclusive);
 
 /// Thread-safe alternative to \c sys::fs::unlockFile. This does not support all
 /// the platforms that \c sys::fs::lockFile does, so keep it in the CAS library
@@ -42,7 +44,7 @@ std::error_code unlockFileThreadSafe(int FD);
 /// library for now.
 std::error_code tryLockFileThreadSafe(
     int FD, std::chrono::milliseconds Timeout = std::chrono::milliseconds(0),
-    bool Exclusive = true);
+    llvm::sys::fs::LockKind Kind = llvm::sys::fs::LockKind::Exclusive);
 
 /// Allocate space for the file \p FD on disk, if the filesystem supports it.
 ///
