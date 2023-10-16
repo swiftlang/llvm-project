56767e9dfe7ad3d0bc4e36fe290f8661a708ba97
Shubham Sandeep Rastogi <srastogi22@apple.com>
2023-10-16 13:08:12 -0700

    Create one CASObject per die in debug info section and see if it gives better results

--- llvm/lib/MCCAS/MCCASObjectV1.cpp
+++ llvm/lib/MCCAS/MCCASObjectV1.cpp
@@ -2929,29 +2929,30 @@ Expected<DIEDataRef> DIEDataRef::create(MCCASBuilder &MB,
 
 // Returns true if DIE should be placed in a separate CAS block.
 static bool shouldCreateSeparateBlockFor(DWARFDie &DIE) {
-  dwarf::Tag Tag = DIE.getTag();
+  return true;
+  // dwarf::Tag Tag = DIE.getTag();
 
-  if (Tag == dwarf::Tag::DW_TAG_subprogram) {
-    // Only split on subprogram definitions, so look for low_pc.
-    for (const DWARFAttribute &AttrValue : DIE.attributes())
-      if (AttrValue.Attr == dwarf::Attribute::DW_AT_low_pc)
-        return true;
-    return false;
-  }
+  // if (Tag == dwarf::Tag::DW_TAG_subprogram) {
+  //   // Only split on subprogram definitions, so look for low_pc.
+  //   for (const DWARFAttribute &AttrValue : DIE.attributes())
+  //     if (AttrValue.Attr == dwarf::Attribute::DW_AT_low_pc)
+  //       return true;
+  //   return false;
+  // }
 
-  if (Tag == dwarf::Tag::DW_TAG_enumeration_type)
-    return true;
+  // if (Tag == dwarf::Tag::DW_TAG_enumeration_type)
+  //   return true;
 
-  if (Tag == dwarf::Tag::DW_TAG_structure_type ||
-      Tag == dwarf::Tag::DW_TAG_class_type) {
-    // Don't split on declarations, as these are short
-    for (const DWARFAttribute &AttrValue : DIE.attributes())
-      if (AttrValue.Attr == dwarf::Attribute::DW_AT_declaration)
-        return false;
-    return true;
-  }
+  // if (Tag == dwarf::Tag::DW_TAG_structure_type ||
+  //     Tag == dwarf::Tag::DW_TAG_class_type) {
+  //   // Don't split on declarations, as these are short
+  //   for (const DWARFAttribute &AttrValue : DIE.attributes())
+  //     if (AttrValue.Attr == dwarf::Attribute::DW_AT_declaration)
+  //       return false;
+  //   return true;
+  // }
 
-  return false;
+  // return false;
 }
 
 static void writeDIEAttrs(DWARFDie &DIE, ArrayRef<char> DebugInfoData,
