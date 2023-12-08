70668d5f87809dcb4a6b5abd7c265ae49e6bad6a
Dave Lee <davelee.com@gmail.com>
2023-12-08 10:44:46 -0800

    Test is Darwin-only for now

--- lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedSummary.py
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedSummary.py
@@ -1,9 +1,11 @@
 import lldb
+from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
 
 class TestCase(TestBase):
+    @skipUnlessDarwin
     def test(self):
         self.build()
         lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.c"))
