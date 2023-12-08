aad1da8843c42beaf62889e0ccfa546f7392990a
Dave Lee <davelee.com@gmail.com>
2023-12-08 10:18:54 -0800

    Add unit test

---
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedSummary.py
@@ -0,0 +1,10 @@
+import lldb
+from lldbsuite.test.lldbtest import *
+from lldbsuite.test import lldbutil
+
+
+class TestCase(TestBase):
+    def test(self):
+        self.build()
+        lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.c"))
+        self.expect("v player", substrs=['"Dirk" (41)'])
