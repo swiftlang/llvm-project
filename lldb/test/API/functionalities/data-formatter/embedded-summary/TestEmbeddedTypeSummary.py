18850a093a4cc11fe82e53a6f05ba3a428638488
Dave Lee <davelee.com@gmail.com>
2024-02-06 06:54:13 -0800

    Remove test debugging

--- lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedTypeSummary.py
+++ lldb/test/API/functionalities/data-formatter/embedded-summary/TestEmbeddedTypeSummary.py
@@ -7,10 +7,7 @@ from lldbsuite.test import lldbutil
 class TestCase(TestBase):
     @skipUnlessDarwin
     def test(self):
-        self.runCmd("log enable lldb formatters")
         self.build()
         lldbutil.run_to_source_breakpoint(self, "break here", lldb.SBFileSpec("main.c"))
         self.expect("v player", substrs=['"Dirk" (41)'])
-        self.runCmd("type summary list", trace=True)
-        self.runCmd("type summary info layer", trace=True)
         self.expect("v layer", substrs=['"crust" (3)'])
