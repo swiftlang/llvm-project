88243a2935753a6e79517c20fbfcdb4ff4abeb2f
Steven Wu <stevenwu@apple.com>
2024-08-12 12:46:55 -0700

    [Tests] Fix downstream tests after upstream #99538
    Update tests with `getelementptr inbound nuw`
    
    rdar://133702351

--- clang/test/CodeGen/ptrauth-blocks.c
+++ clang/test/CodeGen/ptrauth-blocks.c
@@ -9,7 +9,7 @@ void (^globalblock)(void) = ^{};
 // CHECK-LABEL: define void @test_block_call()
 void test_block_call() {
   // CHECK:      [[T0:%.*]] = load ptr, ptr @blockptr,
-  // CHECK-NEXT: [[FNADDR:%.*]] = getelementptr inbounds {{.*}}, ptr [[T0]], i32 0, i32 3
+  // CHECK-NEXT: [[FNADDR:%.*]] = getelementptr inbounds nuw {{.*}}, ptr [[T0]], i32 0, i32 3
   // CHECK-NEXT: [[T1:%.*]] = load ptr, ptr [[FNADDR]],
   // CHECK-NEXT: [[DISC:%.*]] = ptrtoint ptr [[FNADDR]] to i64
   // CHECK-NEXT: call void [[T1]](ptr noundef [[T0]]) [ "ptrauth"(i32 0, i64 [[DISC]]) ]
@@ -22,7 +22,7 @@ void use_block(int (^)(void));
 void test_block_literal(int i) {
   // CHECK:      [[I:%.*]] = alloca i32,
   // CHECK-NEXT: [[BLOCK:%.*]] = alloca [[BLOCK_T:.*]], align
-  // CHECK:      [[FNPTRADDR:%.*]] = getelementptr inbounds [[BLOCK_T]], ptr [[BLOCK]], i32 0, i32 3
+  // CHECK:      [[FNPTRADDR:%.*]] = getelementptr inbounds nuw [[BLOCK_T]], ptr [[BLOCK]], i32 0, i32 3
   // CHECK-NEXT: [[DISCRIMINATOR:%.*]] = ptrtoint ptr [[FNPTRADDR]] to i64
   // CHECK-NEXT: [[SIGNED:%.*]] = call i64 @llvm.ptrauth.sign(i64 ptrtoint (ptr {{@.*}} to i64), i32 0, i64 [[DISCRIMINATOR]])
   // CHECK-NEXT: [[T0:%.*]] = inttoptr i64 [[SIGNED]] to ptr
