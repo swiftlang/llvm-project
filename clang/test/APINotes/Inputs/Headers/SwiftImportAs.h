6e90682d68fab1aa5c1f543552317ec4e718bc0e
Doug Gregor <dgregor@apple.com>
2025-08-12 09:17:18 -0700

    [API notes] Add a test for the use of SwiftConformsTo on a typedef

--- clang/test/APINotes/Inputs/Headers/SwiftImportAs.h
+++ clang/test/APINotes/Inputs/Headers/SwiftImportAs.h
@@ -25,3 +25,5 @@ struct OpaqueRefCountedType; // redeclaration
 
 inline void ORCRetain(struct OpaqueRefCountedType *x);
 inline void ORCRelease(struct OpaqueRefCountedType *x);
+
+typedef unsigned WrappedOptions;
