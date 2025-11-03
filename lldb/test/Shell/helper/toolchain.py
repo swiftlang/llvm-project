dc031b67fb44f33c8c3feafebb751255572c5610
Leonard Grey <leonard@leonardgrey.com>
2025-11-03 15:25:42 -0500

    Allow setting extra Swift flags on Windows

--- lldb/test/Shell/helper/toolchain.py
+++ lldb/test/Shell/helper/toolchain.py
@@ -260,6 +260,10 @@ def use_support_substitutions(config):
     swift_driver_args = []
     if platform.system() in ["Darwin"]:
         swift_args += ["-sdk", sdk_path]
+    if sys.platform == "win32":
+        swift_driver_extra_args = getattr(config, "swift_driver_extra_args_win", [])
+        if swift_driver_extra_args:
+            swift_args += swift_driver_extra_args
     tools = [
         ToolSubst(
             "%target-swiftc",
