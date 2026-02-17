93df58529890aca183a3f47348cd4dbe53515b0f
Hiroshi Yamauchi <hjyamauchi@gmail.com>
2026-02-17 11:01:19 -0800

    Revert "Fix an assertion failure with CAS and pch for Windows"
    This reverts commit 9324f5110ed16c690378cf95d02400787736be85.

--- clang/lib/Frontend/FrontendAction.cpp
+++ clang/lib/Frontend/FrontendAction.cpp
@@ -1441,7 +1441,7 @@ bool FrontendAction::BeginSourceFile(CompilerInstance &CI,
           if (!PCHFile)
             return reportError(PCHFile.takeError());
           PCHCASID = PCHFile->getID().toString();
-          PCHPath = PCHBuffer->getBufferIdentifier();
+          PCHPath = PCHCASID;
         } else {
           PCHPath = CI.getPreprocessorOpts().ImplicitPCHInclude;
           DisableValidation =
