855225d5417eea47948ac892ba3bc3cca85ac552
Raphael Isemann <rise@apple.com>
2026-04-09 14:37:11 +0100

    [lldb][test] Fix call signature in TestAppleSimulatorOSType (#191185)
    Commit 918e446ef28ac97df20d6ef2bd50c78e2fe903ac removed the
    stderr_lines_to_read argument but didn't adjust the call site in
    TestAppleSimulatorOSType. This patch just removes the extra arg here
    too.
    
    (cherry picked from commit 93845b8ef4790a853068706200d417ef3184a098)

--- lldb/test/API/tools/lldb-server/TestAppleSimulatorOSType.py
+++ lldb/test/API/tools/lldb-server/TestAppleSimulatorOSType.py
@@ -10,8 +10,6 @@ import re
 
 
 class TestAppleSimulatorOSType(gdbremote_testcase.GdbRemoteTestCaseBase):
-    # Number of stderr lines to read from the simctl output.
-    READ_LINES = 10
 
     def check_simulator_ostype(self, sdk, platform_name, arch=platform.machine()):
         # Get simulator
@@ -56,7 +54,6 @@ class TestAppleSimulatorOSType(gdbremote_testcase.GdbRemoteTestCaseBase):
             deviceUDID,
             self.getBuildArtifact(exe_name),
             ["print-pid", "sleep:10"],
-            self.READ_LINES,
             [r"PID: (.*)"],
             self.trace,
         )
