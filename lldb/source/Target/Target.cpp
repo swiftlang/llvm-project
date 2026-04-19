1b16c74e12e433876166d3205d1122931be6b9ff
Michael Buch <michaelbuch12@gmail.com>
2026-04-19 06:54:45 +0100

    [lldb][Target] Move Debugger::GetSafeAutoLoadPaths into Target (#192703)
    Required for https://github.com/llvm/llvm-project/pull/191454
    
    We want to make the `Target`/`Platform` define commonly used default
    paths (without configuring them in CMake). This is easiest if this logic
    lived in `Target` (since then we have access to the associated
    `Platform`).
    
    (cherry picked from commit 404b75651f7220e9de1801fa987ccf92c1fc95bb)

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -6325,6 +6325,18 @@ void Target::DumpSectionLoadList(Stream &s) {
   GetSectionLoadList().Dump(s, this);
 }
 
+FileSpecList Target::GetSafeAutoLoadPaths() const {
+  FileSpecList fspecs = Debugger::GetDefaultSafeAutoLoadPaths();
+
+#ifndef NDEBUG
+  for (const auto &fspec :
+       TestingProperties::GetGlobalTestingProperties().GetSafeAutoLoadPaths())
+    fspecs.Append(fspec);
+#endif
+
+  return fspecs;
+}
+
 // FIXME: the language plugin should expression options dynamically and
 // we should validate here (by asking the language plugin) that the options
 // being set/retrieved are actually valid options.
