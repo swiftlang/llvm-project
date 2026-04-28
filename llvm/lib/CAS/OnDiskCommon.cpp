99a6a18483fa47d400e696355383cbfe5c2b4f6f
Steven Wu <stevenwu@apple.com>
2026-04-28 10:40:22 -0700

    [CAS] Retry blocking syscalls on EINTR
    Wrap blocking syscalls used by LLVMCAS inside RetryAfterSignal so they
    are retried on EINTR instead of surfacing as error or silently dropping
    actions due to failed to acquire locks.

--- llvm/lib/CAS/OnDiskCommon.cpp
+++ llvm/lib/CAS/OnDiskCommon.cpp
@@ -9,6 +9,7 @@
 #include "OnDiskCommon.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Config/config.h"
+#include "llvm/Support/Errno.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
@@ -67,7 +68,7 @@ void cas::ondisk::setMaxMappingSize(uint64_t Size) {
 
 std::error_code cas::ondisk::lockFileThreadSafe(int FD, bool Exclusive) {
 #if HAVE_FLOCK
-  if (flock(FD, Exclusive ? LOCK_EX : LOCK_SH) == 0)
+  if (sys::RetryAfterSignal(-1, flock, FD, Exclusive ? LOCK_EX : LOCK_SH) == 0)
     return std::error_code();
   return std::error_code(errno, std::generic_category());
 #elif defined(_WIN32)
@@ -80,7 +81,7 @@ std::error_code cas::ondisk::lockFileThreadSafe(int FD, bool Exclusive) {
 
 std::error_code cas::ondisk::unlockFileThreadSafe(int FD) {
 #if HAVE_FLOCK
-  if (flock(FD, LOCK_UN) == 0)
+  if (sys::RetryAfterSignal(-1, flock, FD, LOCK_UN) == 0)
     return std::error_code();
   return std::error_code(errno, std::generic_category());
 #elif defined(_WIN32)
@@ -98,7 +99,8 @@ cas::ondisk::tryLockFileThreadSafe(int FD, std::chrono::milliseconds Timeout,
   auto Start = std::chrono::steady_clock::now();
   auto End = Start + Timeout;
   do {
-    if (flock(FD, (Exclusive ? LOCK_EX : LOCK_SH) | LOCK_NB) == 0)
+    if (sys::RetryAfterSignal(-1, flock, FD,
+                              (Exclusive ? LOCK_EX : LOCK_SH) | LOCK_NB) == 0)
       return std::error_code();
     int Error = errno;
     if (Error == EWOULDBLOCK) {
@@ -137,7 +139,11 @@ Expected<size_t> cas::ondisk::preallocateFileTail(int FD, size_t CurrentSize, si
   };
 #if defined(HAVE_POSIX_FALLOCATE)
   // Note: posix_fallocate returns its error directly, not via errno.
-  if (int Err = posix_fallocate(FD, CurrentSize, NewSize - CurrentSize))
+  int Err;
+  do {
+    Err = posix_fallocate(FD, CurrentSize, NewSize - CurrentSize);
+  } while (Err == EINTR);
+  if (Err)
     return CreateError(std::error_code(Err, std::generic_category()));
   return NewSize;
 #elif defined(__APPLE__)
@@ -147,7 +153,7 @@ Expected<size_t> cas::ondisk::preallocateFileTail(int FD, size_t CurrentSize, si
   FAlloc.fst_offset = 0;
   FAlloc.fst_length = NewSize - CurrentSize;
   FAlloc.fst_bytesalloc = 0;
-  if (fcntl(FD, F_PREALLOCATE, &FAlloc))
+  if (sys::RetryAfterSignal(-1, ::fcntl, FD, F_PREALLOCATE, &FAlloc) == -1)
     return CreateError(errnoAsErrorCode());
   assert(CurrentSize + FAlloc.fst_bytesalloc >= NewSize);
   return CurrentSize + FAlloc.fst_bytesalloc;
