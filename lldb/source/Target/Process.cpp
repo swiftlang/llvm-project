b4260a8f65f120ebe435ecb3dccd4e017bc215a5
Charles Zablit <c_zablit@apple.com>
2026-06-01 18:15:02 +0100

    [lldb][windows] fix breakpoint delay during launch/attach

--- lldb/source/Target/Process.cpp
+++ lldb/source/Target/Process.cpp
@@ -1772,7 +1772,9 @@ llvm::Error Process::ExecuteBreakpointSiteAction(BreakpointSite &site,
                                                  BreakpointAction action,
                                                  bool forbid_delay) {
   // Breakpoints immediately affect running processes, so do not delay them.
-  forbid_delay |= StateIsRunningState(GetPrivateState());
+  forbid_delay |= StateIsRunningState(GetPrivateState()) ||
+                  GetPublicState() == eStateLaunching ||
+                  GetPublicState() == eStateAttaching;
 
   if (forbid_delay)
     if (llvm::Error E = FlushDelayedBreakpoints())
