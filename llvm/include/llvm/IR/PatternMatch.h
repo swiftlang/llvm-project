nekoshirro/Alchemist-LLVM

[PatternMatch] Add capturing m_Phi matcher (NFC) (#208949)
Add a new capturing m_Phi matcher and use it in some patterns instead of
manually casting/checking if operand is a phi.

PR: https://github.com/llvm/llvm-project/pull/208949
Signed-off-by: Hafidz Muzakky <ais.muzakky@gmail.com>

llvm/include/llvm/IR/PatternMatch.h
@@ -879,6 +879,9 @@ m_WithOverflowInst(const WithOverflowInst *&I) {
   return I;
 }
 
+/// Match a PHI node, capturing it if we match.
+inline match_bind<PHINode> m_Phi(PHINode *&PN) { return PN; }
+
 /// Match an UndefValue, capturing the value if we match.
 inline match_bind<UndefValue> m_UndefValue(UndefValue *&U) { return U; }
 

swiftlang/llvm-project

(cherry picked from commit 25a5b7869cd13698f7556af6b4084cca835ae988)
[Cherry-pick adaptation: upstream defines m_Phi via the renamed
match_bind<> helper; this fork still uses bind_ty<>, so m_Phi is
defined as bind_ty<PHINode> here.]

llvm/include/llvm/IR/PatternMatch.h
@@ -862,6 +862,9 @@ m_WithOverflowInst(const WithOverflowInst *&I) {
   return I;
 }
 
+/// Match a PHI node, capturing it if we match.
+inline bind_ty<PHINode> m_Phi(PHINode *&PN) { return PN; }
+
 /// Match an UndefValue, capturing the value if we match.
 inline bind_ty<UndefValue> m_UndefValue(UndefValue *&U) { return U; }
 